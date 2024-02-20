39d7c17a9784c95d5963fb1c3b24c22a506ae0e0
Arya Priema <99aryaprima@gmail.com>
2024-02-20 19:21:43 +0700

    feat: Dashboard/Admin/ProjectController
    - create
    - store

--- app/Http/Controllers/Dashboard/Admin/ProjectController.php
+++ app/Http/Controllers/Dashboard/Admin/ProjectController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Dashboard\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
+use App\Models\ProjectCategory;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -25,17 +27,48 @@ class ProjectController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): Response
     {
-        //
+        $categories = ProjectCategory::all();
+
+        return response()
+            ->view('dashboard.admin.projects.create', [
+                "categories" => $categories
+            ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(): RedirectResponse
     {
-        //
+        request()->validate([
+            'title' => 'required|max:74',
+            'description' => 'required|max:199',
+            'client' => 'required|max:74',
+            'technology' => 'required',
+            'is_desktop' => 'required',
+            'project_category_id' => 'required|exists:project_categories,id',
+            'image' => 'required|file|max:1024'
+        ]);
+
+        $technology = json_encode(explode(',', request()->input('technology')));
+        $fileName = time() . request()->file('image')->getClientOriginalName();
+        $path = request()->file('image')->storeAs('images/projects', $fileName, 'public');
+
+        $project = new Project();
+        $project->title = request()->input('title');
+        $project->description = request()->input('description');
+        $project->client = request()->input('client');
+        $project->technology = $technology;
+        $project->is_desktop = request()->input('is_desktop') === "Yes";
+        $project->project_category_id = request()->input('project_category_id');
+        $project->image = $path;
+        $project->save();
+
+        return redirect()
+            ->route('dashboard.projects.index')
+            ->with('success', 'Project Created Successfully.');
     }
 
     /**
---
+++ resources/views/dashboard/admin/projects/create.blade.php
@@ -0,0 +1,117 @@
+@extends('layouts.dashboard.dashboard')
+
+@section('content')
+    <section class="border bg-white rounded shadow-sm">
+        <div class="p-[20px] border-b">
+            <h1 class="font-semibold text-black text-lg">New Project</h1>
+        </div>
+        <div class="p-[20px]">
+            <form action="{{ route('dashboard.projects.store') }}" method="POST" class="flex flex-col gap-[20px]"
+                enctype="multipart/form-data">
+                @csrf
+
+                <!-- Title -->
+                <div class="flex flex-col">
+                    <label for="title" class="font-medium">Title</label>
+                    <input type="text" name="title" id="title" autofocus value="{{ old('title') }}"
+                        placeholder="Title" class="@error('title') dashboard-input__error @else dashboard-input @enderror"
+                        required>
+                    @error('title')
+                        <span class="text-red-600 text-sm">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <!-- Description -->
+                <div class="flex flex-col">
+                    <label for="description" class="font-medium">Description</label>
+                    <textarea name="description" id="description" placeholder="Description"
+                        class="@error('description') dashboard-input__error @else dashboard-input @enderror" required rows="8">{{ old('description') }}</textarea>
+                    @error('description')
+                        <span class="text-red-600 text-sm">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <!-- Client -->
+                <div class="flex flex-col">
+                    <label for="client" class="font-medium">Client</label>
+                    <input type="text" name="client" id="client" autofocus value="{{ old('client') }}"
+                        placeholder="Client" class="@error('client') dashboard-input__error @else dashboard-input @enderror"
+                        required>
+                    @error('client')
+                        <span class="text-red-600 text-sm">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <!-- Technology -->
+                <div class="flex flex-col">
+                    <label for="technology" class="font-medium">Technology</label>
+                    <input type="text" name="technology" id="technology" autofocus value="{{ old('technology') }}"
+                        placeholder="Technology"
+                        class="@error('technology') dashboard-input__error @else dashboard-input @enderror" required>
+                    @error('technology')
+                        <span class="text-red-600 text-sm">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <!-- Is Desktop -->
+                <div class="flex flex-col">
+                    <label for="is_desktop" class="font-medium">Is Desktop</label>
+                    <select name="is_desktop" id="is_desktop"
+                        class="@error('is_desktop') dashboard-input__error @else dashboard-input @enderror" required>
+                        <option value="1">Yes</option>
+                        <option value="0">No</option>
+                    </select>
+
+                    @error('is_desktop')
+                        <span class="text-red-600 text-sm">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <!-- Project Category Id -->
+                <div class="flex flex-col">
+                    <label for="project_category_id" class="font-medium">Choose Category</label>
+                    <select name="project_category_id" id="project_category_id"
+                        class="@error('project_category_id') dashboard-input__error @else dashboard-input @enderror"
+                        required>
+                        @foreach ($categories as $category)
+                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                        @endforeach
+                    </select>
+
+                    @error('project_category_id')
+                        <span class="text-red-600 text-sm">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <!-- Image -->
+                <div class="flex flex-col">
+                    <label for="image" class="font-medium">Image</label>
+                    <input onchange="loadFile(event)" type="file" name="image" id="image"
+                        value="{{ old('image') }}" placeholder="image"
+                        class="@error('image') dashboard-input__error @else dashboard-input @enderror" required>
+                    @error('image')
+                        <span class="text-red-600 text-sm">{{ $message }}</span>
+                    @enderror
+
+                    <!-- Image Preview -->
+                    <img id="image-preview" class="w-fit h-fit">
+                </div>
+
+                <!-- Submit -->
+                <button
+                    class="bg-blue-500 hover:bg-blue-500/90 disabled:bg-blue-500/60 text-white py-2 px-8 w-fit rounded font-medium outline-none">Save</button>
+                <!-- End Of Submit -->
+            </form>
+        </div>
+    </section>
+@endsection
+
+@section('scripts')
+    <script>
+        function loadFile(event) {
+            const imagePreview = document.getElementById('image-preview');
+            imagePreview.src = URL.createObjectURL(event.target.files[0]);
+            imagePreview.alt = 'image-preview';
+        }
+    </script>
+@endsection
