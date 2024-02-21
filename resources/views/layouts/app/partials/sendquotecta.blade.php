37091eef3c348b7c3dea9284cf650edf3505a16a
Arya Priema <99aryaprima@gmail.com>
2024-02-21 20:46:09 +0700

    feat: sendquotecta.blade.php - responsive

--- resources/views/layouts/app/partials/sendquotecta.blade.php
+++ resources/views/layouts/app/partials/sendquotecta.blade.php
@@ -1,9 +1,13 @@
-<section class="relative w-[1025px] mx-auto pt-[80px]">
-    <div class="py-[40px] border-y-[1px] border-y-white/10">
-        <div class="flex items-center justify-between">
-            <h1 class="text-white font-bold leading-[48px] text-[30px]">We've prepared everything, it's time<br /> for
+<section class="relative max-w-[1025px] mx-auto pt-[80px]">
+    <div class="py-[40px] border-y-[1px] border-y-white/10 mx-10 lg:mx-0">
+        <div class="flex items-center justify-between flex-col lg:flex-row gap-y-7 lg:gap-y-0">
+            <h1
+                class="text-white font-bold leading-[36px] sm:leading-[40px] lg:leading-[48px] text-center lg:text-start text-[22px] sm:text-[26px] lg:text-[30px]">
+                We've
+                prepared everything, it's
+                time<br /> for
                 you to tell the problem</h1>
-            <div class="flex items-center gap-x-[10px]">
+            <div class="flex items-center flex-col sm:flex-row gap-[10px]">
                 <x-button-send-quote />
                 <a href="/" class="w-[162px] h-[52px] grid place-items-center bg-white/10 text-white rouned-3">Ask
                     Us</a>
