f4e227258ba3cca395772bb8e63069ee5d241ab7
Arya Priema <99aryaprima@gmail.com>
2024-02-21 22:29:53 +0700

    feat: home/partials/testimonials.blade.php - responsive

--- public/js/home.js
+++ public/js/home.js
@@ -16,12 +16,20 @@ sponsorsSplide.mount(window.splide.Extensions);
 
 const testimonialsCard = document.getElementById("testimonials-card");
 let testimonialsSplide = new Splide(testimonialsCard, {
+    type: "loop",
     arrows: false,
     perPage: 2,
-    padding: "4.5rem",
+    padding: "4rem",
     rewind: true,
     focus: "center",
     omitEnd: true,
+    snap: true,
+    breakpoints: {
+        768: {
+            perPage: 1,
+            padding: "0rem",
+        },
+    },
 });
 
 testimonialsSplide.mount();
--- resources/views/home/partials/testimonials.blade.php
+++ resources/views/home/partials/testimonials.blade.php
@@ -1,20 +1,20 @@
-<section class="relative w-[1025px] mx-auto pt-[150px]">
+<section class="relative max-w-[1025px] mx-auto pt-[150px]">
     <div
         class="bg-[#16FCD2] w-[438.93px] h-[431.03px] rotate-[14.13deg] absolute z-0 -right-[100px] bottom-0 rounded-full opacity-[10%] blur-3xl">
     </div>
 
     <div class="relative z-10 flex flex-col">
-        <div class="flex text-center items-center flex-col gap-y-[16px]">
+        <div class="flex text-center items-center flex-col gap-y-[16px] px-10 lg:px-0">
             <x-green-title title="TESTIMONIAL" />
             <x-description-bold>
                 What do our clients say that we<br /> never let down?
             </x-description-bold>
         </div>
-        <div class="splide" id="testimonials-card">
+        <div class="splide px-5 lg:px-0" id="testimonials-card">
             <div class="splide__track pb-[80px] py-[150px]">
                 <ul class="splide__list">
                     @foreach ($testimonials as $testimonial)
-                        <li class="splide__slide flex justify-center">
+                        <li class="splide__slide px-[10px] flex justify-center">
                             <x-testimonial-card :image="$testimonial->image" :name="$testimonial->name" :companyname="$testimonial->company_name" :content="$testimonial->content"
                                 class="testimonial-card" />
                         </li>
