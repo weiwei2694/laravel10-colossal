a7d1cbb85bc139935a8708ed1907fe7e259e3f86
Arya Priema <99aryaprima@gmail.com>
2024-02-21 21:37:34 +0700

    feat: home/partials/our-team.blade.php -- responsive

--- resources/views/home/partials/our-team.blade.php
+++ resources/views/home/partials/our-team.blade.php
@@ -1,4 +1,4 @@
-<section class="relative w-[1025px] mx-auto py-[150px]">
+<section class="relative max-w-[1025px] mx-auto py-[150px] px-10 lg:px-0">
     <div
         class="bg-[#6016FC] w-[356.61px] h-[350.19px] rotate-[14.13deg] absolute z-0 right-0 top-[50px] rounded-full opacity-[15%] blur-3xl">
     </div>
@@ -6,18 +6,19 @@
         class="bg-[#FC165B] w-[438.93px] h-[431.03px] rotate-[14.13deg] absolute z-0 right-[140px] top-[200px] rounded-full opacity-[15%] blur-3xl">
     </div>
 
-    <div class="flex justify-between items-center relative z-10">
-        <div class="flex flex-col gap-[17px]">
+    <div
+        class="flex justify-between items-start lg:items-center flex-col sm:flex-col-reverse lg:flex-row gap-y-12 lg:gap-y-0 relative z-10">
+        <div class="flex flex-col gap-y-[12px] sm:gap-y-[17px]">
             <x-green-title title="OUR TEAM" />
             <x-description-bold>
-                We're a team of designers,<br /> engineers and analysts
+                We're a team of designers, engineers and analysts
             </x-description-bold>
             <x-description-medium>
-                Our team consists of many creative people. We are<br /> committed to maintaining quality work as well
-                as<br /> speed. These creative people work together to create<br /> maximum work results.
+                Our team consists of many creative people. We are committed to maintaining quality work as well
+                as speed. These creative people work together to create maximum work results.
             </x-description-medium>
             <a href="/"
-                class="mt-[40px] w-[200px] h-[52px] rounded-[3px] bg-white/10 hover:bg-white/5 transition text-white grid place-items-center font-semibold text-[16px]">See
+                class="mt-0 sm:mt-[40px] w-[200px] h-[52px] rounded-[3px] bg-white/10 hover:bg-white/5 transition text-white grid place-items-center font-semibold text-[16px]">See
                 Our Teams</a>
         </div>
         <img src="{{ asset('/assets/our-team-illustration.png') }}" alt="Our Team Illustration" />
