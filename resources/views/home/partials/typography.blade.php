97cb8a410d7fcd803fba1919e61ab49fb8f00aa0
Arya Priema <99aryaprima@gmail.com>
2024-02-21 21:03:59 +0700

    feat: home/partials/typography.blade.php - responsive

--- resources/views/home/partials/typography.blade.php
+++ resources/views/home/partials/typography.blade.php
@@ -1,21 +1,25 @@
-<section class="relative w-[1025px] mx-auto pt-[175px] flex flex-col justify-center text-center">
-    <div class="flex flex-col gap-y-[40px]">
-        <div class="flex flex-col gap-y-[30px]">
+<section class="relative max-w-[1025px] mx-auto pt-[175px] flex flex-col justify-center text-center px-10 lg:px-0">
+    <div class="flex flex-col gap-y-[25px] sm:gap-y-[40px]">
+        <div class="flex flex-col items-center gap-y-[15px] sm:gap-y-[30px]">
             <h2 class="text-green font-semibold text-[16px] tracking-[3px] leading-[32px]">CLIENT-DEVELOPMENT
                 DRIVEN
             </h2>
-            <h1 class="text-white font-bold text-[36px] leading-[54px]">We Design. We Develop. We Ship.<br /> In
+            <h1
+                class="text-white font-bold text-[24px] sm:text-[32px] lg:text-[36px] leading-[44px] sm:leading-[54px] max-w-fit md:max-w-[600px]">
+                We Design. We Develop. We Ship. In
                 The
                 Same Day.</h1>
-            <p class="text-white/60 font-medium leading-[32px] text-[16px]">We are committed to not making
+            <p
+                class="text-white/60 font-medium leading-[22px] sm:leading-[32px] text-[11px] sm:text-[14px] lg:text-[16px] max-w-fit md:max-w-[600px]">
+                We are committed to not making
                 clients
                 wait.
                 We
-                will deliver the work<br /> as quickly
+                will deliver the work as quickly
                 as possible. Even
-                on the same day. Even so, we do not<br /> reduce the quality of our work.</p>
+                on the same day. Even so, we do not reduce the quality of our work.</p>
         </div>
-        <div class="flex items-center gap-x-[30px] justify-center">
+        <div class="flex items-center gap-[10px] sm:gap-[30px] justify-center flex-col sm:flex-row">
             <x-button-send-quote />
             <a href="/"
                 class="w-[200px] h-[52px] rounded-[3px] bg-white/10 hover:bg-white/5 transition text-white grid place-items-center font-semibold text-[16px]">Learn
