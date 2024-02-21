12ac45cb9b835067de9dce8963c179043d150fed
Arya Priema <99aryaprima@gmail.com>
2024-02-21 21:08:17 +0700

    feat: home/partials/features.blade.php - responsive

--- resources/views/home/partials/features.blade.php
+++ resources/views/home/partials/features.blade.php
@@ -1,5 +1,5 @@
-<section class="relative w-[1025px] mx-auto pt-[150px]">
-    <div class="grid grid-cols-3 gap-x-[25px]">
+<section class="relative max-w-[1025px] mx-auto pt-[150px] px-10 lg:px-0">
+    <div class="grid grid-cols-1 lg:grid-cols-3 gap-[25px]">
         @foreach ($features as $feature)
             <div class="bg-white/5 w-full h-fit p-[35px] flex flex-col gap-y-[20px]">
                 <div class="bg-white/5 rounded-3 w-fit h-fit p-[15px]">
