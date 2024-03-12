2d4cf22836eb156cbe48a101d0ad7c139784a9bb
Arya Priema <99aryaprima@gmail.com>
2024-03-12 15:08:44 +0700

    update(styles): sendquotecta.blade.php - round decoration

--- resources/views/layouts/app/partials/sendquotecta.blade.php
+++ resources/views/layouts/app/partials/sendquotecta.blade.php
@@ -1,4 +1,11 @@
 <section class="relative max-w-[1025px] mx-auto pt-[80px]">
+    <div
+        class="w-[55.09px] h-[55.09px] bg-gradient-to-br from-[#66FFA3] to-[#009C3E] rounded-full blur-sm absolute left-[20px] lg:-left-[140px] -bottom-[70px]">
+    </div>
+    <div
+        class="w-[46.94px] h-[46.94px] bg-gradient-to-br from-[#FF81A6] to-[#FC165B] rounded-full blur-sm absolute right-[60px] lg:-right-[140px] top-[60px]">
+    </div>
+
     <div class="py-[40px] border-y-[1px] border-y-white/10 mx-10 lg:mx-0">
         <div class="flex items-center justify-between flex-col lg:flex-row gap-y-7 lg:gap-y-0">
             <h1
