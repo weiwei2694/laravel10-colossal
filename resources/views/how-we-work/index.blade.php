d4445a92fe06fab29ebc18f8e5abfb0268fe7dc1
Arya Priema <99aryaprima@gmail.com>
2024-03-13 15:20:19 +0700

    feat: how-we-work/partials-step-01

--- resources/views/how-we-work/index.blade.php
+++ resources/views/how-we-work/index.blade.php
@@ -3,4 +3,6 @@
 @section('content')
     {{-- Section - How We Work --}}
     @include('how-we-work.partials.how-we-work')
+    {{-- Section - Step 01 --}}
+    @include('how-we-work.partials.step-01')
 @endsection
---
+++ resources/views/how-we-work/partials/step-01.blade.php
@@ -0,0 +1,20 @@
+<section class="relative max-w-[1025px] mx-auto pt-[75px] pb-[150px] px-10 lg:px-0 border-t border-t-white/10">
+    <div
+        class="flex justify-between items-start lg:items-center flex-col lg:flex-row gap-y-12 lg:gap-y-0 gap-x-0 lg:gap-x-12 relative z-10">
+        <img src="{{ asset('/assets/discuss-illustration.png') }}" alt="Discuss Illustration" />
+        <div class="flex flex-col gap-y-[25px] sm:gap-y-[40px]">
+            <div class="flex flex-col gap-y-[6px]">
+                <x-green-title title="STEP 01" />
+                <x-description-bold class="w-fit lg:max-w-[476px]">
+                    Let's talk about your company's problems first
+                </x-description-bold>
+            </div>
+            <x-description-medium class="w-fit lg:max-w-[502px]">
+                After submitting the quote form, we will review the data, then we will contact you. You can discuss with
+                our team regarding your problem and find a solution to that problem.<br /><br />
+
+                In this step, you will discuss what application to build.
+            </x-description-medium>
+        </div>
+    </div>
+</section>
