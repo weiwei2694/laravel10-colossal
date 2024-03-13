f511bf85f976a341aa9ae46178de6e646033c0c2
Arya Priema <99aryaprima@gmail.com>
2024-03-13 14:20:51 +0700

    feat: Dashboard/Admin/FaqController@{create,store}

--- app/Http/Controllers/Dashboard/Admin/FaqController.php
+++ app/Http/Controllers/Dashboard/Admin/FaqController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Dashboard\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Faq;
+use App\Models\FaqCategory;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Response;
 
 class FaqController extends Controller
@@ -15,5 +17,32 @@ class FaqController extends Controller
 
         return response()
             ->view('dashboard.admin.faqs.index', compact('faqs'));
+    }
+
+    public function create(): Response
+    {
+        $categories = FaqCategory::all();
+
+        return response()
+            ->view('dashboard.admin.faqs.create', compact('categories'));
+    }
+
+    public function store(): RedirectResponse
+    {
+        request()->validate([
+            'question' => 'required|max:74',
+            'answer' => 'required|max:499',
+            'faq_category_id' => 'required|exists:faq_categories,id',
+        ]);
+
+        $faq = new Faq();
+        $faq->question = request()->input('question');
+        $faq->answer = request()->input('answer');
+        $faq->faq_category_id = request()->input('faq_category_id');
+        $faq->save();
+
+        return redirect()
+            ->route('dashboard.faqs.index')
+            ->with('success', 'Faq Created Successfully.');
     }
 }
---
+++ resources/views/dashboard/admin/faqs/create.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.dashboard.dashboard')
+
+@section('content')
+    <section class="border bg-white rounded shadow-sm">
+        <div class="p-[20px] border-b">
+            <h1 class="font-semibold text-black text-lg">New Faq</h1>
+        </div>
+        <div class="p-[20px]">
+            <form action="{{ route('dashboard.faqs.store') }}" method="POST" class="flex flex-col gap-[20px]">
+                @csrf
+
+                <!-- Question -->
+                <div class="flex flex-col">
+                    <label for="question" class="font-medium">Question</label>
+                    <input type="text" name="question" id="question" autofocus value="{{ old('question') }}"
+                        placeholder="Question"
+                        class="@error('question') dashboard-input__error @else dashboard-input @enderror" required>
+                    @error('question')
+                        <span class="text-red-600 text-sm">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <!-- Answer -->
+                <div class="flex flex-col">
+                    <label for="answer" class="font-medium">Answer</label>
+                    <textarea name="answer" id="answer" placeholder="Answer"
+                        class="@error('answer') dashboard-input__error @else dashboard-input @enderror" required rows="8">{{ old('answer') }}</textarea>
+                    @error('answer')
+                        <span class="text-red-600 text-sm">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <!-- Faq Category Id -->
+                <div class="flex flex-col">
+                    <label for="faq_category_id" class="font-medium">Choose Category</label>
+                    <select name="faq_category_id" id="faq_category_id"
+                        class="@error('faq_category_id') dashboard-input__error @else dashboard-input @enderror" required>
+                        @foreach ($categories as $category)
+                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                        @endforeach
+                    </select>
+
+                    @error('faq_category_id')
+                        <span class="text-red-600 text-sm">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <!-- Submit -->
+                <button
+                    class="bg-blue-500 hover:bg-blue-500/90 disabled:bg-blue-500/60 text-white py-2 px-8 w-fit rounded font-medium outline-none">Save</button>
+                <!-- End Of Submit -->
+            </form>
+        </div>
+    </section>
+@endsection
