30455a3dd74fdfb3e297ef112e7e603a4b379c7a
Arya Priema <99aryaprima@gmail.com>
2024-03-13 15:34:38 +0700

    feat: service-detail.blade.php - redirect to service-detail

--- resources/views/components/service-detail.blade.php
+++ resources/views/components/service-detail.blade.php
@@ -1,3 +1,3 @@
-<a href="/"
+<a href="{{ route('services.show') }}"
     class="text-white font-semibold text-[16px] tracking-[32%] flex items-center gap-x-[20px] justify-end">Service
     Detail <img src="{{ asset('/assets/arrow-right.png') }}" alt="Arrow Right" width="20" height="20" /></a>
