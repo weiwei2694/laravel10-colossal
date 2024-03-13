f0ce0c553a6ce0441c977a4d7daed7a7898137f6
Arya Priema <99aryaprima@gmail.com>
2024-03-13 14:28:34 +0700

    feat: Dashboard/Admin/FaqController@{edit, update}

--- app/Http/Controllers/Dashboard/Admin/FaqController.php
+++ app/Http/Controllers/Dashboard/Admin/FaqController.php
@@ -46,6 +46,32 @@ class FaqController extends Controller
             ->with('success', 'Faq Created Successfully.');
     }
 
+    public function edit(Faq $faq): Response
+    {
+        $categories = FaqCategory::all();
+
+        return response()
+            ->view('dashboard.admin.faqs.edit', compact('categories', 'faq'));
+    }
+
+    public function update(Faq $faq): RedirectResponse
+    {
+        request()->validate([
+            'question' => 'required|max:74',
+            'answer' => 'required|max:499',
+            'faq_category_id' => 'required|exists:faq_categories,id',
+        ]);
+
+        $faq->question = request()->input('question');
+        $faq->answer = request()->input('answer');
+        $faq->faq_category_id = request()->input('faq_category_id');
+        $faq->save();
+
+        return redirect()
+            ->route('dashboard.faqs.index')
+            ->with('success', 'Faq Updated Successfully.');
+    }
+
     public function destroy(Faq $faq): RedirectResponse
     {
         $faq->delete();
