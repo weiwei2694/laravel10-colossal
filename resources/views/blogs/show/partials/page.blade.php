d9e8c9cfa0695dd55b086f25a1adc1d9e81a0f3e
Arya Priema <99aryaprima@gmail.com>
2024-03-17 17:37:39 +0700

    update(styles): blogs/show/partials/page - responsive card-author

--- resources/views/blogs/show/partials/page.blade.php
+++ resources/views/blogs/show/partials/page.blade.php
@@ -41,31 +41,38 @@
             </div>
             {{-- Card Author --}}
             <div class="mt-12 mx-auto w-full md:w-[674px] p-12 rounded-[3px] bg-white/5">
-                <div class="flex flex-row gap-x-12">
+                <div class="flex flex-col md:flex-row gap-y-4 lg:gap-y-0 gap-x-0 lg:gap-x-12">
                     <img src="{{ asset('/storage/' . $post->user->image) }}" alt="{{ $post->user->name }}"
                         class="w-[101px] h-[101px] object-cover rounded-full">
                     <div class="flex flex-col gap-y-6 p-2">
-                        <div class="flex justify-between items-center">
+                        <div
+                            class="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-y-3 sm:gap-y-0">
                             <div class="flex flex-col gap-y-1">
                                 <h4 class="font-semibold text-white text-[20px]">{{ $post->user->name }}</h4>
                                 <p class="text-white/60 font-semibold text-[12px]">{{ $post->user->role }}</p>
                             </div>
                             <div class="flex flex-row gap-x-2">
-                                <a href="{{ $post->user->twitter }}"
-                                    class="w-[40px] h-[40px] grid place-items-center rounded-[100px] bg-white/5"
-                                    target="_blank">
-                                    <img src="{{ asset('/assets/twitter.png') }}" alt="Twitter">
-                                </a>
-                                <a href="{{ $post->user->facebook }}"
-                                    class="w-[40px] h-[40px] grid place-items-center rounded-[100px] bg-white/5"
-                                    target="_blank">
-                                    <img src="{{ asset('/assets/facebook (1).png') }}" alt="Facebook">
-                                </a>
-                                <a href="{{ $post->user->linkedin }}"
-                                    class="w-[40px] h-[40px] grid place-items-center rounded-[100px] bg-white/5"
-                                    target="_blank">
-                                    <img src="{{ asset('/assets/linkedin.png') }}" alt="Linkedin">
-                                </a>
+                                @if ($post->user->twitter)
+                                    <a href="{{ $post->user->twitter }}"
+                                        class="w-[40px] h-[40px] grid place-items-center rounded-[100px] bg-white/5"
+                                        target="_blank">
+                                        <img src="{{ asset('/assets/twitter.png') }}" alt="Twitter">
+                                    </a>
+                                @endif
+                                @if ($post->user->facebook)
+                                    <a href="{{ $post->user->facebook }}"
+                                        class="w-[40px] h-[40px] grid place-items-center rounded-[100px] bg-white/5"
+                                        target="_blank">
+                                        <img src="{{ asset('/assets/facebook (1).png') }}" alt="Facebook">
+                                    </a>
+                                @endif
+                                @if ($post->user->linkedin)
+                                    <a href="{{ $post->user->linkedin }}"
+                                        class="w-[40px] h-[40px] grid place-items-center rounded-[100px] bg-white/5"
+                                        target="_blank">
+                                        <img src="{{ asset('/assets/linkedin.png') }}" alt="Linkedin">
+                                    </a>
+                                @endif
                             </div>
                         </div>
                         <p class="text-white font-medium leading-[34px] text-[16px]">
