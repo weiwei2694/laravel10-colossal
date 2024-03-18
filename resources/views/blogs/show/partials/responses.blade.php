3199d4d03bcbfbf460d82e4c861f3e35a17e11d3
Arya Priema <99aryaprima@gmail.com>
2024-03-18 12:35:45 +0700

    update(styles): blogs/show/partaisl/responses - display for tablet and mobile

--- resources/views/blogs/show/partials/responses.blade.php
+++ resources/views/blogs/show/partials/responses.blade.php
@@ -1,18 +1,20 @@
-<section class="relative max-w-[1025px] mx-auto pt-[150px] pb-[40px] px-10 lg:px-0 border-t border-t-white/10">
+<section class="relative max-w-[1025px] mx-auto pt-[150px] pb-[40px] border-t border-t-white/10">
     <div class="flex flex-col gap-y-[65px] mx-auto w-full md:w-[678px]">
-        <x-description-bold class="text-center">Responses</x-description-bold>
-        <div id="data-wrapper" class="flex flex-col">
-            @if (!empty($post->comments->toArray()))
-                @include('components.cards-comment')
-            @else
-                <x-description-medium class="text-center">No comments yet ...</x-description-medium>
-            @endif
+        <div class="px-10 lg:px-0 flex flex-col gap-y-[65px]">
+            <x-description-bold class="text-center">Responses</x-description-bold>
+            <div id="data-wrapper" class="flex flex-col">
+                @if (!empty($post->comments->toArray()))
+                    @include('components.cards-comment')
+                @else
+                    <x-description-medium class="text-center">No comments yet ...</x-description-medium>
+                @endif
+            </div>
         </div>
         <div class="bg-white/5 rounded-[3px] py-12 px-10">
             <form class="flex flex-col gap-y-10" action="{{ route('blogs.create-comment', $post->id) }}" method="POST">
                 @csrf
 
-                <div class="flex flex-col lg:flex-row gap-x-6">
+                <div class="flex flex-col lg:flex-row gap-y-10 lg:gap-y-0 gap-x-0 lg:gap-x-6">
                     <div class="flex flex-col gap-y-2 w-full">
                         <label for="name" class="font-normal text-white/60 text-[14px]">Name</label>
                         <input type="text" id="name" name="name"
@@ -36,8 +38,7 @@
                     <label for="content" class="font-normal text-white/60 text-[14px]">Content</label>
                     <textarea
                         class="outline-none border border-white/10 bg-transparent text-white py-2 px-6 rounded-[3px] w-full resize-none @error('content') 'input-error' @enderror"
-                        id="content" name="content" rows="6" required>
-                    </textarea>
+                        id="content" name="content" rows="6" required></textarea>
                     @error('content')
                         <span class="text-xs text-red-500">{{ $message }}</span>
                     @enderror
--- resources/views/components/cards-comment.blade.php
+++ resources/views/components/cards-comment.blade.php
@@ -1,8 +1,8 @@
 @foreach ($post->comments as $comment)
-    <div class="flex flex-row gap-x-8 border-b border-b-white/10 py-12">
+    <div class="flex flex-col sm:flex-row gap-6 sm:gap-8 border-b border-b-white/10 py-12">
         <img src="{{ asset('/assets/comment-pp.jpg') }}" alt="Comment PP"
             class="w-[50px] h-[50px] rounded-[100px] object-cover">
-        <div class="flex flex-col gap-y-6">
+        <div class="flex flex-col gap-y-4 sm:gap-y-6">
             <div>
                 <h4 class="font-semibold text-[20px] text-white">{{ $comment->name }}</h4>
                 <p class="font-normal text-white/60 text-[14px]">{{ $comment->created_at->diffForHumans() }}</p>
