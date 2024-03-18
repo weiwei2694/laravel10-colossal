d620408715ddd56d9d40c45e38d822ac9e872d0c
Arya Priema <99aryaprima@gmail.com>
2024-03-18 12:54:34 +0700

    fix: public/js/home - adds validation to each retrieved element, to avoid error messages

--- public/js/home.js
+++ public/js/home.js
@@ -1,35 +1,37 @@
 const sponsorsCard = document.getElementById("sponsors-card");
-let sponsorsSplide = new Splide(sponsorsCard, {
-    type: "loop",
-    arrows: false,
-    perPage: 3,
-    autoScroll: {
-        speed: 3,
-        pauseOnHover: false,
-    },
-    pagination: false,
-    omitEnd: true,
-    snap: true,
-});
-
-sponsorsSplide.mount(window.splide.Extensions);
+if (sponsorsCard) {
+    let sponsorsSplide = new Splide(sponsorsCard, {
+        type: "loop",
+        arrows: false,
+        perPage: 3,
+        autoScroll: {
+            speed: 3,
+            pauseOnHover: false,
+        },
+        pagination: false,
+        omitEnd: true,
+        snap: true,
+    });
+    sponsorsSplide.mount(window.splide.Extensions);
+}
 
 const testimonialsCard = document.getElementById("testimonials-card");
-let testimonialsSplide = new Splide(testimonialsCard, {
-    type: "loop",
-    arrows: false,
-    perPage: 2,
-    padding: "4rem",
-    rewind: true,
-    focus: "center",
-    omitEnd: true,
-    snap: true,
-    breakpoints: {
-        768: {
-            perPage: 1,
-            padding: "0rem",
+if (testimonialsCard) {
+    let testimonialsSplide = new Splide(testimonialsCard, {
+        type: "loop",
+        arrows: false,
+        perPage: 2,
+        padding: "4rem",
+        rewind: true,
+        focus: "center",
+        omitEnd: true,
+        snap: true,
+        breakpoints: {
+            768: {
+                perPage: 1,
+                padding: "0rem",
+            },
         },
-    },
-});
-
-testimonialsSplide.mount();
+    });
+    testimonialsSplide.mount();
+}
