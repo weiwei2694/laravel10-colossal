82c3625bd2332b6e014bf6361322af77968a5079
Arya Priema <99aryaprima@gmail.com>
2024-03-18 13:45:26 +0700

    feat: dashboard/admin/project-category/create

--- app/Http/Controllers/Dashboard/Admin/ProjectCategoryController.php
+++ app/Http/Controllers/Dashboard/Admin/ProjectCategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Dashboard\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\ProjectCategory;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Response;
 
 class ProjectCategoryController extends Controller
@@ -14,5 +15,26 @@ class ProjectCategoryController extends Controller
 
         return response()
             ->view('dashboard.admin.project-category.index', compact('projectCategories'));
+    }
+
+    public function create(): Response
+    {
+        return response()
+            ->view('dashboard.admin.project-category.create');
+    }
+
+    public function store(): RedirectResponse
+    {
+        request()->validate([
+            'name' => 'required|max:74|unique:project_categories,name',
+        ]);
+
+        $projectCategory = new ProjectCategory();
+        $projectCategory->name = request()->input('name');
+        $projectCategory->save();
+
+        return redirect()
+            ->route('dashboard.project-categories.index')
+            ->with('success', 'Project category created successfully.');
     }
 }
