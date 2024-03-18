8cbb0fe8c11c2ae38f4c6ee0c4a3552790ef1523
Arya Priema <99aryaprima@gmail.com>
2024-03-18 12:22:45 +0700

    feat: blogs/show/partials/responses
    - displays original data
    - add validation if comments data does not yet exist

--- resources/views/blogs/show/partials/responses.blade.php
+++ resources/views/blogs/show/partials/responses.blade.php
@@ -2,34 +2,11 @@
     <div class="flex flex-col gap-y-[65px] mx-auto w-full md:w-[678px]">
         <x-description-bold class="text-center">Responses</x-description-bold>
         <div id="data-wrapper" class="flex flex-col">
-            <div class="flex flex-row gap-x-8 border-b border-b-white/10 py-12">
-                <img src="{{ asset('/assets/comment-pp.jpg') }}" alt="Comment PP"
-                    class="w-[50px] h-[50px] rounded-[100px] object-cover">
-                <div class="flex flex-col gap-y-6">
-                    <div>
-                        <h4 class="font-semibold text-[20px] text-white">Robert Fox</h4>
-                        <p class="font-normal text-white/60 text-[14px]">8 min ago</p>
-                    </div>
-                    <p class="font-medium text-white text-[16px] leading-[32px]">Moveth fish were living fruitful
-                        created from dry his
-                        one dry sea you're. It greater doesn't
-                        replenish replenish divide moveth. They're under itself without given a male light years fruit
-                        rule sea moveth.</p>
-                </div>
-            </div>
-            <div class="flex flex-row gap-x-8 border-b border-b-white/10 py-12">
-                <img src="{{ asset('/assets/comment-pp.jpg') }}" alt="Comment PP"
-                    class="w-[50px] h-[50px] rounded-[100px] object-cover">
-                <div class="flex flex-col gap-y-6">
-                    <div>
-                        <h4 class="font-semibold text-[20px] text-white">Ralph Edwards</h4>
-                        <p class="font-normal text-white/60 text-[14px]">20 min ago</p>
-                    </div>
-                    <p class="font-medium text-white text-[16px] leading-[32px]">Upon have, rule grass made fish Day
-                        seed. In fowl his life third appear they're sixth heaven fifth fourth darkness. Man. Lights
-                        heaven. Fowl yielding two female Bring bring, rule fifth Upon under male.</p>
-                </div>
-            </div>
+            @if (!empty($post->comments->toArray()))
+                @include('components.cards-comment')
+            @else
+                <x-description-medium class="text-center">No comments yet ...</x-description-medium>
+            @endif
         </div>
         <div class="bg-white/5 rounded-[3px] py-12 px-10">
             <form class="flex flex-col gap-y-10" action="{{ route('blogs.create-comment', $post->id) }}" method="POST">
---
+++ resources/views/components/cards-comment.blade.php
@@ -0,0 +1,13 @@
+@foreach ($post->comments as $comment)
+    <div class="flex flex-row gap-x-8 border-b border-b-white/10 py-12">
+        <img src="{{ asset('/assets/comment-pp.jpg') }}" alt="Comment PP"
+            class="w-[50px] h-[50px] rounded-[100px] object-cover">
+        <div class="flex flex-col gap-y-6">
+            <div>
+                <h4 class="font-semibold text-[20px] text-white">{{ $comment->name }}</h4>
+                <p class="font-normal text-white/60 text-[14px]">{{ $comment->created_at->diffForHumans() }}</p>
+            </div>
+            <p class="font-medium text-white text-[16px] leading-[32px]">{{ $comment->content }}</p>
+        </div>
+    </div>
+@endforeach
