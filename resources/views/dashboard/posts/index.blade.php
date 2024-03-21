509f29510bd501ce092cee267e2063af0ad883bc
Arya Priema <99aryaprima@gmail.com>
2024-03-21 15:09:10 +0700

    feat(refactor): dashboard/posts/index - handle empty state and display posts list

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -31,98 +31,109 @@
                 </div>
             </div>
             <div class="p-[20px]">
-                <div class="flex flex-col">
-                    <div class="-m-1.5 overflow-x-auto">
-                        <div class="p-1.5 min-w-full inline-block align-middle">
-                            <div class="overflow-hidden">
-                                <table class="min-w-full divide-y divide-gray-200">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col"
-                                                class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Id
-                                            </th>
-                                            @can('is_admin')
+                @if ($posts->isEmpty())
+                    <div class="grid place-items-center">
+                        <h4 class="text-card-dark/60 font-semibold">No Posts ...</h4>
+                    </div>
+                @else
+                    <div class="flex flex-col">
+                        <div class="-m-1.5 overflow-x-auto">
+                            <div class="p-1.5 min-w-full inline-block align-middle">
+                                <div class="overflow-hidden">
+                                    <table class="min-w-full divide-y divide-gray-200">
+                                        <thead>
+                                            <tr>
                                                 <th scope="col"
                                                     class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
-                                                    User Id
+                                                    Id
                                                 </th>
-                                            @endcan
-                                            <th scope="col"
-                                                class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
-                                                Title
-                                            </th>
-                                            <th scope="col"
-                                                class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
-                                                Subtitle
-                                            </th>
-                                            <th scope="col"
-                                                class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
-                                                Reading
-                                                Time
-                                            </th>
-                                            <th scope="col"
-                                                class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
-                                                Tags
-                                            </th>
-                                            <th scope="col"
-                                                class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
-                                                Action
-                                            </th>
-                                        </tr>
-                                    </thead>
-                                    <tbody class="divide-y divide-gray-200">
-                                        @foreach ($posts as $post)
-                                            <tr>
-                                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">
-                                                    {{ $post->id }}</td>
                                                 @can('is_admin')
-                                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">
-                                                        {{ $post->user_id }}</td>
+                                                    <th scope="col"
+                                                        class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                                                        User Id
+                                                    </th>
                                                 @endcan
-                                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">
-                                                    {{ $post->title }}</td>
-                                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
-                                                    {{ $post->subtitle }}</td>
-                                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
-                                                    {{ $post->reading_time }} min</td>
-                                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
-                                                    @foreach (json_decode($post->tags) as $tag)
-                                                        <span
-                                                            class="py-1 px-2 rounded bg-blue-500 text-white font-medium">{{ $tag }}</span>
-                                                    @endforeach
-                                                </td>
-                                                <td class="px-6 py-4 whitespace-nowrap text-start text-sm font-medium">
-                                                    <div class="flex items-center gap-x-[20px]">
-                                                        <div>
-                                                            <form id="delete-form"
-                                                                action="{{ route('dashboard.posts.destroy', '__id__') }}"
-                                                                method="POST">
-                                                                @csrf
-                                                                @method('DELETE')
-                                                            </form>
-
-                                                            <button onclick="deletePost({{ $post->id }})" type="button"
-                                                                class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-gray-600 hover:text-gray-800 disabled:opacity-50 disabled:pointer-events-none">
-                                                                Delete
-                                                            </button>
-                                                        </div>
-                                                        <x-action-item
-                                                            url="{{ route('dashboard.posts.show', $post->id) }}">View</x-action-item>
-                                                        <x-action-item
-                                                            url="{{ route('dashboard.posts.edit', $post->id) }}">Edit</x-action-item>
-                                                    </div>
-                                                </td>
+                                                <th scope="col"
+                                                    class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                                                    Title
+                                                </th>
+                                                <th scope="col"
+                                                    class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                                                    Subtitle
+                                                </th>
+                                                <th scope="col"
+                                                    class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                                                    Reading
+                                                    Time
+                                                </th>
+                                                <th scope="col"
+                                                    class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                                                    Tags
+                                                </th>
+                                                <th scope="col"
+                                                    class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                                                    Action
+                                                </th>
                                             </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
+                                        </thead>
+                                        <tbody class="divide-y divide-gray-200">
+                                            @foreach ($posts as $post)
+                                                <tr>
+                                                    <td
+                                                        class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">
+                                                        {{ $post->id }}</td>
+                                                    @can('is_admin')
+                                                        <td
+                                                            class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">
+                                                            {{ $post->user_id }}</td>
+                                                    @endcan
+                                                    <td
+                                                        class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">
+                                                        {{ $post->title }}</td>
+                                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
+                                                        {{ $post->subtitle }}</td>
+                                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
+                                                        {{ $post->reading_time }} min</td>
+                                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
+                                                        @foreach (json_decode($post->tags) as $tag)
+                                                            <span
+                                                                class="py-1 px-2 rounded bg-blue-500 text-white font-medium">{{ $tag }}</span>
+                                                        @endforeach
+                                                    </td>
+                                                    <td class="px-6 py-4 whitespace-nowrap text-start text-sm font-medium">
+                                                        <div class="flex items-center gap-x-[20px]">
+                                                            <div>
+                                                                <form id="delete-form"
+                                                                    action="{{ route('dashboard.posts.destroy', '__id__') }}"
+                                                                    method="POST">
+                                                                    @csrf
+                                                                    @method('DELETE')
+                                                                </form>
+
+                                                                <button onclick="deletePost({{ $post->id }})"
+                                                                    type="button"
+                                                                    class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-gray-600 hover:text-gray-800 disabled:opacity-50 disabled:pointer-events-none">
+                                                                    Delete
+                                                                </button>
+                                                            </div>
+                                                            <x-action-item
+                                                                url="{{ route('dashboard.posts.show', $post->id) }}">View</x-action-item>
+                                                            <x-action-item
+                                                                url="{{ route('dashboard.posts.edit', $post->id) }}">Edit</x-action-item>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="flex justify-start pt-[20px]">
-                    {{ $posts->links() }}
-                </div>
+                    <div class="flex justify-start pt-[20px]">
+                        {{ $posts->links() }}
+                    </div>
+                @endif
             </div>
         </section>
     </div>
