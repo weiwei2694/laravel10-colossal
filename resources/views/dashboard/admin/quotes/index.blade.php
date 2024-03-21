61bbd4ee4a3e4f158b9edfb93a24f964674946ef
Arya Priema <99aryaprima@gmail.com>
2024-03-21 14:40:01 +0700

    feat: dashboard/admin/quotes/index - filter quotes based on is_read field

--- resources/views/dashboard/admin/quotes/index.blade.php
+++ resources/views/dashboard/admin/quotes/index.blade.php
@@ -1,106 +1,150 @@
 @extends('layouts.dashboard.dashboard')
 
 @section('content')
-    <section class="border bg-white rounded shadow-sm">
-        <div class="p-[20px] border-b">
+    <div class="flex flex-col gap-y-3">
+        <section class="border bg-white rounded shadow-sm p-[20px] max-xl:hidden xl:block">
             <h1 class="font-semibold text-black text-lg">All Quotes</h1>
-        </div>
-        <div class="p-[20px]">
-            <div class="flex flex-col">
-                <div class="-m-1.5 overflow-x-auto">
-                    <div class="p-1.5 min-w-full inline-block align-middle">
-                        <div class="overflow-hidden">
-                            <table class="min-w-full divide-y divide-gray-200">
-                                <thead>
-                                    <tr>
-                                        <th scope="col"
-                                            class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Id
-                                        </th>
-                                        <th scope="col"
-                                            class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
-                                            Name
-                                        </th>
-                                        <th scope="col"
-                                            class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
-                                            Company
-                                        </th>
-                                        <th scope="col"
-                                            class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
-                                            Company Size
-                                        </th>
-                                        <th scope="col"
-                                            class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
-                                            Is Read
-                                        </th>
-                                        <th scope="col"
-                                            class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Action
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody class="divide-y divide-gray-200">
-                                    @foreach ($quotes as $quote)
-                                        <tr>
-                                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">
-                                                {{ $quote->id }}</td>
-                                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">
-                                                {{ $quote->name }}</td>
-                                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">
-                                                {{ $quote->company }}</td>
-                                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">
-                                                {{ $quote->company_size }}</td>
-                                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">
-                                                @if ($quote->is_read)
-                                                    <span class="py-1 px-3 rounded-[3px] bg-blue-500 text-white">Yes</span>
-                                                @else
-                                                    <span class="py-1 px-4 rounded-[3px] bg-red-500 text-white">No</span>
-                                                @endif
-                                            <td class="px-6 py-4 whitespace-nowrap text-start text-sm font-medium">
-                                                <div class="flex items-center gap-x-[20px]">
-                                                    <div>
-                                                        <form id="delete-form"
-                                                            action="{{ route('dashboard.quotes.destroy', '__id__') }}"
-                                                            method="POST">
-                                                            @csrf
-                                                            @method('DELETE')
-                                                        </form>
+        </section>
+        <section class="border bg-white rounded shadow-sm">
+            <div class="p-[20px] border-b">
+                {{-- desktop --}}
+                <form id="filterForm" action="{{ route('dashboard.quotes.index') }}" method="GET"
+                    class="max-xl:hidden xl:flex flex-row justify-between gap-x-20">
+                    <div>
+                        {{-- TODO: search filter --}}
+                    </div>
+                    <div class="flex flex-row gap-x-[20px]">
+                        <div class="flex flex-col gap-y-2">
+                            <label for="is_read" class="font-medium text-card-dark/60">Is Read</label>
+                            <select name="is_read" id="is_read" class="outline-none p-2 border-2 rounded-lg"
+                                class="font-semibold">
+                                <option value="all" class="font-medium text-card-dark"
+                                    @if (request('is_read') == 'all') selected @endif>
+                                    All
+                                </option>
+                                <option value="yes" class="font-medium text-card-dark"
+                                    @if (request('is_read') == 'yes') selected @endif>
+                                    Yes
+                                </option>
+                                <option value="no" class="font-medium text-card-dark"
+                                    @if (request('is_read') == 'no') selected @endif>
+                                    No
+                                </option>
+                            </select>
+                        </div>
+                        {{-- TODO: company size filter --}}
+                    </div>
+                </form>
 
-                                                        <button onclick="deletePost({{ $quote->id }})" type="button"
-                                                            class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-gray-600 hover:text-gray-800 disabled:opacity-50 disabled:pointer-events-none">
-                                                            Delete
-                                                        </button>
-                                                    </div>
-                                                    <x-action-item
-                                                        url="{{ route('dashboard.quotes.show', $quote->id) }}">View</x-action-item>
-                                                    @if (!$quote->is_read)
+                {{-- mobile - tablet --}}
+                <div class="max-xl:flex flex-row justify-between xl:hidden">
+                    <h1 class="font-semibold text-black text-lg">All Quotes</h1>
+                    <button id="open-sidebar-filter" type="button" class="text-lg text-gray-600">
+                        <i class="ri-menu-line"></i>
+                    </button>
+                </div>
+            </div>
+            <div class="p-[20px]">
+                <div class="flex flex-col">
+                    <div class="-m-1.5 overflow-x-auto">
+                        <div class="p-1.5 min-w-full inline-block align-middle">
+                            <div class="overflow-hidden">
+                                <table class="min-w-full divide-y divide-gray-200">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col"
+                                                class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Id
+                                            </th>
+                                            <th scope="col"
+                                                class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                                                Name
+                                            </th>
+                                            <th scope="col"
+                                                class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                                                Company
+                                            </th>
+                                            <th scope="col"
+                                                class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                                                Company Size
+                                            </th>
+                                            <th scope="col"
+                                                class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                                                Is Read
+                                            </th>
+                                            <th scope="col"
+                                                class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                                                Action
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody class="divide-y divide-gray-200">
+                                        @foreach ($quotes as $quote)
+                                            <tr>
+                                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">
+                                                    {{ $quote->id }}</td>
+                                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">
+                                                    {{ $quote->name }}</td>
+                                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">
+                                                    {{ $quote->company }}</td>
+                                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">
+                                                    {{ $quote->company_size }}</td>
+                                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">
+                                                    @if ($quote->is_read)
+                                                        <span
+                                                            class="py-1 px-3 rounded-[3px] bg-blue-500 text-white">Yes</span>
+                                                    @else
+                                                        <span
+                                                            class="py-1 px-4 rounded-[3px] bg-red-500 text-white">No</span>
+                                                    @endif
+                                                <td class="px-6 py-4 whitespace-nowrap text-start text-sm font-medium">
+                                                    <div class="flex items-center gap-x-[20px]">
                                                         <div>
-                                                            <form id="update-form"
-                                                                action="{{ route('dashboard.quotes.update', '__id__') }}"
+                                                            <form id="delete-form"
+                                                                action="{{ route('dashboard.quotes.destroy', '__id__') }}"
                                                                 method="POST">
                                                                 @csrf
-                                                                @method('PUT')
+                                                                @method('DELETE')
                                                             </form>
 
-                                                            <button onclick="markAsRead({{ $quote->id }})" type="button"
+                                                            <button onclick="deletePost({{ $quote->id }})" type="button"
                                                                 class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-gray-600 hover:text-gray-800 disabled:opacity-50 disabled:pointer-events-none">
-                                                                Mark As Read
+                                                                Delete
                                                             </button>
                                                         </div>
-                                                    @endif
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
+                                                        <x-action-item
+                                                            url="{{ route('dashboard.quotes.show', $quote->id) }}">View</x-action-item>
+                                                        @if (!$quote->is_read)
+                                                            <div>
+                                                                <form id="update-form"
+                                                                    action="{{ route('dashboard.quotes.update', '__id__') }}"
+                                                                    method="POST">
+                                                                    @csrf
+                                                                    @method('PUT')
+                                                                </form>
+
+                                                                <button onclick="markAsRead({{ $quote->id }})"
+                                                                    type="button"
+                                                                    class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-gray-600 hover:text-gray-800 disabled:opacity-50 disabled:pointer-events-none">
+                                                                    Mark As Read
+                                                                </button>
+                                                            </div>
+                                                        @endif
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <div class="flex justify-start pt-[20px]">
+                    {{ $quotes->links() }}
+                </div>
             </div>
-            <div class="flex justify-start pt-[20px]">
-                {{ $quotes->links() }}
-            </div>
-        </div>
-    </section>
+        </section>
+    </div>
 @endsection
 
 @section('scripts')
@@ -149,5 +193,14 @@
                 }
             });
         }
+
+        // Filter Form - desktop
+        document.querySelectorAll('select').forEach(function(select) {
+            select.addEventListener('change', function() {
+                if (this.closest('#filterForm')) {
+                    document.getElementById('filterForm').submit();
+                }
+            });
+        });
     </script>
 @endsection
