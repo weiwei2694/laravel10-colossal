ae9e6fbaf66c46d921257e6a2267ea4098fcd250
Arya Priema <99aryaprima@gmail.com>
2024-03-21 15:24:56 +0700

    feat(refactor): about/index - Add conditional rendering to components requiring data

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -5,8 +5,12 @@
     @include('about.partials.about')
     {{-- Section - Statistics --}}
     @include('about.partials.statistics')
-    {{-- Section - Sponsors --}}
-    @include('home.partials.sponsors')
-    {{-- Section - Our Team --}}
-    @include('about.partials.our-team')
+    @if (!$sponsors->isEmpty())
+        {{-- Section - Sponsors --}}
+        @include('home.partials.sponsors')
+    @endif
+    @if (!$ourTeams->isEmpty())
+        {{-- Section - Our Team --}}
+        @include('about.partials.our-team')
+    @endif
 @endsection
