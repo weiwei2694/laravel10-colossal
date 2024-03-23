61e61dae37738eef26ba2808500821d057b93eda
Arya Priema <99aryaprima@gmail.com>
2024-03-23 17:59:50 +0700

    update(styles): dashboard/admin/quotes/*
    - input
    - textarea
    - validation error
    - label
    - form

--- resources/views/dashboard/admin/quotes/show.blade.php
+++ resources/views/dashboard/admin/quotes/show.blade.php
@@ -9,57 +9,57 @@
             <div class="flex flex-col gap-[20px]">
                 <!-- Id -->
                 <div class="flex flex-col">
-                    <label for="id" class="font-medium">Id</label>
-                    <input type="text" name="id" id="id" value="{{ $quote->id }}" class="dashboard-input"
+                    <label for="id" class="dashboard__label">Id</label>
+                    <input type="text" name="id" id="id" value="{{ $quote->id }}" class="dashboard__input"
                         disabled>
                 </div>
 
                 <!-- Name -->
                 <div class="flex flex-col">
-                    <label for="name" class="font-medium">Name</label>
-                    <input type="text" name="name" id="name" value="{{ $quote->name }}" class="dashboard-input"
+                    <label for="name" class="dashboard__label">Name</label>
+                    <input type="text" name="name" id="name" value="{{ $quote->name }}" class="dashboard__input"
                         disabled>
                 </div>
 
                 <!-- Company -->
                 <div class="flex flex-col">
-                    <label for="company" class="font-medium">Company</label>
+                    <label for="company" class="dashboard__label">Company</label>
                     <input type="text" name="company" id="company" value="{{ $quote->company }}"
-                        class="dashboard-input" disabled>
+                        class="dashboard__input" disabled>
                 </div>
 
                 <!-- Company Size -->
                 <div class="flex flex-col">
-                    <label for="company_size" class="font-medium">Company Size</label>
+                    <label for="company_size" class="dashboard__label">Company Size</label>
                     <input type="text" name="company_size" id="company_size" value="{{ $quote->company_size }}"
-                        class="dashboard-input" disabled>
+                        class="dashboard__input" disabled>
                 </div>
 
                 <!-- Problem -->
                 <div class="flex flex-col">
-                    <label for="problem" class="font-medium">Problem</label>
-                    <textarea name="problem" id="problem" class="dashboard-input" rows="8" disabled>{{ $quote->problem }}</textarea>
+                    <label for="problem" class="dashboard__label">Problem</label>
+                    <textarea name="problem" id="problem" class="dashboard__textarea" rows="8" disabled>{{ $quote->problem }}</textarea>
                 </div>
 
                 <!-- Is Read -->
                 <div class="flex flex-col">
-                    <label for="is_read" class="font-medium">Is Read</label>
+                    <label for="is_read" class="dashboard__label">Is Read</label>
                     <input type="text" name="is_read" id="is_read" value="{{ $quote->is_read }}"
-                        class="dashboard-input" disabled>
+                        class="dashboard__input" disabled>
                 </div>
 
                 <!-- Created At -->
                 <div class="flex flex-col">
-                    <label for="created_at" class="font-medium">Created At</label>
+                    <label for="created_at" class="dashboard__label">Created At</label>
                     <input type="text" name="created_at" id="created_at" value="{{ $quote->created_at }}"
-                        class="dashboard-input" disabled>
+                        class="dashboard__input" disabled>
                 </div>
 
                 <!-- Updated At -->
                 <div class="flex flex-col">
-                    <label for="updated_at" class="font-medium">Updated At</label>
+                    <label for="updated_at" class="dashboard__label">Updated At</label>
                     <input type="text" name="updated_at" id="updated_at"
-                        value="{{ $quote->updated_at->diffForHumans() }}" class="dashboard-input" disabled>
+                        value="{{ $quote->updated_at->diffForHumans() }}" class="dashboard__input" disabled>
                 </div>
             </div>
         </div>
