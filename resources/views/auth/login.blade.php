6dfdc3619904bc6188e27ea5e6204c80880dbef0
Arya Priema <99aryaprima@gmail.com>
2024-03-25 18:27:28 +0700

    update(styles): auth/login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,48 +3,84 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+    >
+    <meta
+        http-equiv="X-UA-Compatible"
+        content="ie=edge"
+    >
     <title>Login</title>
 
     <!-- Styles -->
     @vite('resources/css/app.css')
-    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
+    <link
+        href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css"
+        rel="stylesheet"
+    >
 </head>
 
-<body class="bg-card-dark h-screen max-h-screen grid place-items-center">
+<body class="bg-[#0a0a0a] h-[100dvh] max-h-[100dvh] flex justify-center items-center">
     {{-- Main --}}
-    <main class="p-[30px] rounded-[3px] bg-white shadow flex flex-col space-y-[15px] w-[400px]">
-        <h1 class="text-center text-[34px] font-semibold text-[#181818]">Login</h1>
-
-        <form action="{{ route('login') }}" method="POST" class="flex flex-col gap-y-[20px]">
+    <main class="w-full max-w-[560px] flex flex-col gap-y-16 py-[50px] px-[40px]">
+        <h1 class="text-white text-[20px] text-center font-bold">Welcome to Colossal, <span
+                class="text-blue-500">Buddy!</span></h1>
+        <form
+            action="{{ route('login') }}"
+            method="POST"
+            class="flex flex-col gap-y-6"
+        >
             @csrf
 
-            {{-- Email --}}
-            <div class="flex flex-col gap-y-[3px]">
-                <label for="email" class="label">Email</label>
-                <input type="email" placeholder="Email" id="email" name="email" autofocus
-                    class="input @error('email') input-error @enderror" value="{{ old('email') }}" required />
-
+            <div class="flex flex-col gap-y-[5px]">
+                <label
+                    for="email"
+                    class="login__label"
+                >
+                    Email Address
+                </label>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value="{{ old('email') }}"
+                    placeholder="youremail@example.com"
+                    class="login__input @error('email') login__input-error @enderror"
+                    required
+                    autofocus
+                >
                 @error('email')
-                    <p class="text-red-500">{{ $message }}</p>
+                    <span class="login__invalid-feedback">
+                        {{ $message }}
+                    </span>
                 @enderror
             </div>
-
-            {{-- Password --}}
-            <div class="flex flex-col gap-y-[3px]">
-                <label for="password" class="label">Password</label>
-                <input type="password" placeholder="Password" id="password" name="password"
-                    class="input @error('email') input-error @enderror" required />
-
+            <div class="flex flex-col gap-y-[5px]">
+                <label
+                    for="password"
+                    class="login__label"
+                >
+                    Password
+                </label>
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    class="login__input @error('email') login__input-error @enderror"
+                    required
+                >
                 @error('email')
-                    <p class="text-red-500">{{ $message }}</p>
+                    <span class="login__invalid-feedback">
+                        {{ $message }}
+                    </span>
                 @enderror
             </div>
-
-            <button type="submit"
-                class="rounded-[3px] w-full h-[40px] p-[20px] flex items-center justify-center gap-x-[8px] font-sans font-semibold text-[16px] disabled:cursor-not-allowed bg-[#181818] hover:bg-[#181818]/90 text-white disabled:bg-[#181818]/60">
-                Login
+            <button
+                type="submit"
+                class="login__button"
+            >
+                Log In
             </button>
         </form>
     </main>
